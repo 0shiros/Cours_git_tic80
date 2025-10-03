5c80191fe0a3b4905d9736fd07937783bc605d0c
Campo-Sayo Lucas <lcamposayo@gmail.com>
2025-10-03 11:47:03 +0200

    endgame.tic

--- endgame.lua
+++ endgame.lua
@@ -1,8 +1,14 @@
+<<<<<<< HEAD
 
 function endgame()
 
 if var1==var2 & var2==var3 then
      print("Victoire !") 
+     print("+"2*bet + "golds")
+     personnal_golds=personnal_golds+ bet*2
+
 else
     print("Defaite !")
+    print("-"bet + "golds")
 end
+
